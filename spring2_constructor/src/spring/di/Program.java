a49733d65d5039b1bf287c167b956f96d61406d2
sidekava <sidekava@gmail.com>
2020-07-06 23:10:38 +0900

    8강, 9강, 10강 완료. 1. java -> maven project 전환 2. spring library 가져오기 3. applicationcontext를 이용하여 스프링 IoC 컨테이너를 이용하고 객체를 생성 4. 값 형식으로 DI 5. 생성자 DI 6. namespace 개념 간단하게 정립 4.

--- spring2_constructor/src/spring/di/Program.java
+++ spring2_constructor/src/spring/di/Program.java
@@ -3,6 +3,7 @@ package spring.di;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.di.entity.Exam;
 import spring.di.ui.ExamConsole;
 
 public class Program {
@@ -11,6 +12,7 @@ public class Program {
 		
 		/* 스프링에게 지시하는 방법으로 코드를 변
 		Exam exam = new NewlecExam();
+		Exam exam = new NewlecExam(10, 10, 10, 10);
 		ExamConsole console = new GridExamConsole();
 		
 		console.setExam(exam);
@@ -21,6 +23,8 @@ public class Program {
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("spring/di/setting.xml");
 		
+		Exam exam = context.getBean(Exam.class);
+		System.out.println(exam.toString());
 		ExamConsole console = (ExamConsole) context.getBean("console");
 		//ExamConsole console = context.getBean(ExamConsole.class); -> Object형으로 꺼내지므로 캐스팅 필요
 		console.print();
