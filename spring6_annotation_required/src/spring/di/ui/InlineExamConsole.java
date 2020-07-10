234a0a7dd9c14c0e9b6ae84c33ada19d8f1bb642
sidekava <sidekava@gmail.com>
2020-07-10 21:36:59 +0900

    14강 required 사용

--- spring6_annotation_required/src/spring/di/ui/InlineExamConsole.java
+++ spring6_annotation_required/src/spring/di/ui/InlineExamConsole.java
@@ -8,14 +8,16 @@ import spring.di.entity.Exam;
 public class InlineExamConsole implements ExamConsole {
 	
 	/* 여기에 injection을 setter한다. */
+	@Autowired(required=false)
+	@Qualifier("exam2")
 	private Exam exam;
 	
 	public InlineExamConsole() {
 		System.out.println("constructor");
 	}
 	
-	@Autowired
-	public InlineExamConsole(@Qualifier("exam2")Exam exam) {
+	/*@Autowired*/
+	public InlineExamConsole(/*@Qualifier("exam2")*/Exam exam) {
 		System.out.println("overload constructor");
 		this.exam = exam;
 	}
@@ -23,7 +25,9 @@ public class InlineExamConsole implements ExamConsole {
 	@Override
 	public void print() {
 		// TODO Auto-generated method stub
-		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+		if (exam == null) System.out.printf("total is %d, avg is %f\n", 0, 0.0);
+		else System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+		
 	}
 	
 
