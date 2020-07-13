12b39e9d37f7938030fe5698a5642b3f40bf97c2
sidekava <sidekava@gmail.com>
2020-07-13 22:24:27 +0900

    4강 중간 1. 인터페이스와 클래스 생성

--- diPrj/src/diPrj/di/Program.java
+++ diPrj/src/diPrj/di/Program.java
@@ -1,9 +1,25 @@
 package diPrj.di;
 
+import diPrj.di.entity.Exam;
+import diPrj.di.entity.NewlecExam;
+import diPrj.di.ui.ExamConsole;
+import diPrj.di.ui.InlineExamConsole;
+
 public class Program {
 	public static void main (String[] args) {
-		/* data를 갖고있는 entity로서의 클래스와 그 entity를 이용한 출력을 담당하는 클래스 */
+		/* data를 갖고있는 entity로서의 클래스와 그 entity를 이용한 출력을 담당하는 클래스 
+		 */
+		
+		/* Exam exam = new NewlecExam();을 먼저 만들고 인테페이스, 클래스를 만든다.
+		 * 이런 방식을 topdown방식이라 함.
+		 */
 		
 		Exam exam = new NewlecExam();
+		
+		/* exam을 출력해주는 ExamConsole 클래스 추가
+		 * 데이터를 가지고 출력하기 때문에 데이터를 껴 넣어 준다.(exam)
+		 */
+		ExamConsole console = new InlineExamConsole(exam);
+		console.print();
 	}
 }
---
+++ diPrj/src/diPrj/di/ui/InlineExamConsole.java
@@ -0,0 +1,20 @@
+package diPrj.di.ui;
+
+import diPrj.di.entity.Exam;
+
+public class InlineExamConsole implements ExamConsole {
+	
+	private Exam exam;
+	
+	/* 오버로드 생성자를 만든다. 우클릭하여 Source -> Generate Constructor using Fields로 생성한다.
+	 */
+	public InlineExamConsole(Exam exam) {
+		this.exam = exam;
+	}
+
+	@Override
+	public void print() {
+		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+	}
+
+}
