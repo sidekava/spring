4e666e99556046ebe6639d766ae0fc7425ea7559
sidekava <sidekava@gmail.com>
2020-07-20 22:04:56 +0900

    10강 중간 생성자 DI entity class에 생성자를 만들고 xml에 생성자에 세팅을 값을 세팅.

---
+++ diPrj5_constructor_DI/src/diPrj/di/Program.java
@@ -0,0 +1,38 @@
+package diPrj.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import diPrj.di.entity.Exam;
+import diPrj.di.entity.NewlecExam;
+import diPrj.di.ui.ExamConsole;
+import diPrj.di.ui.GridExamConsole;
+import diPrj.di.ui.InlineExamConsole;
+
+public class Program {
+	public static void main (String[] args) {
+		
+		/* ClassPathXmlApplicationContext -> 클래스 기준 경로 현 경로 기준 ex : "../setting.xml"
+		 * FileSystemXmlApplicationContext -> 파일 시스템 기준 경로 ex : C:~~
+		 * XmlWebApplicationContext -> 
+		 * AnnotationConfigApplicationContext -> Web url
+		 */
+		/*
+		Exam exam = new NewlecExam();
+		console.setExam(exam);
+		*/
+		
+		//ApplicationContext를 사용하기 위해선 spring library가 필요
+		//Maven통해 변경 -> project 우클릭 -> Configure -> Convert to Maven Project
+		//
+		ApplicationContext context = 
+				new ClassPathXmlApplicationContext("diPrj/di/setting.xml");
+		
+		Exam exam = context.getBean(Exam.class);
+		System.out.println(exam.toString());
+//		ExamConsole console = new GridExamConsole(); // GridExamConsole이 exam을 조립하고 있다. exam이 dependency가 된 것.
+//		ExamConsole console = (ExamConsole) context.getBean("console");
+		ExamConsole console = context.getBean(ExamConsole.class);
+		console.print();
+	}
+}
