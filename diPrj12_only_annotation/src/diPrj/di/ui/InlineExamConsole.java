ad32e0465f22d935737705812b0f8e21b6eb1db3
sidekava <sidekava@gmail.com>
2020-08-05 22:47:05 +0900

    17강 xml을 없애고 java파일로만 설정.

---
+++ diPrj12_only_annotation/src/diPrj/di/ui/InlineExamConsole.java
@@ -0,0 +1,41 @@
+package diPrj.di.ui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import diPrj.di.entity.Exam;
+
+@Component("console")
+public class InlineExamConsole implements ExamConsole {
+	
+	@Autowired
+//	@Qualifier("exam2")
+	private Exam exam; 
+	
+
+	public InlineExamConsole() {
+		System.out.println("constructor");
+	}
+	
+	public InlineExamConsole(
+			@Qualifier("exam2")Exam exam) {
+		System.out.println("overload");
+		this.exam = exam;
+	}
+
+	@Override
+	public void print() {
+		if (exam == null)
+			System.out.printf("total is %d, avg is %f\n", 0, 0.0);
+		else
+			System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+	}
+
+	@Override
+	public void setExam(Exam exam) {
+		System.out.println("setter");
+		this.exam = exam;
+	}
+
+}
