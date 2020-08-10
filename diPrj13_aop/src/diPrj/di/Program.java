bf0e147a08a4522f1314740406c54a93163424fc
sidekava <sidekava@gmail.com>
2020-08-10 20:18:58 +0900

    18강 복습 AOP 이해 AOP는 관점 지향 프로그래밍 AOP가 스프링은 아니고 스프링이 도와준다.

--- diPrj13_aop/src/diPrj/di/Program.java
+++ diPrj13_aop/src/diPrj/di/Program.java
@@ -8,6 +8,24 @@ import diPrj.di.ui.ExamConsole;
 public class Program {
 	public static void main (String[] args) {
 		
+		/*
+		 * AOP란?
+		 * Aspect Oriented Programming
+		 * 스프링이 아니고 방법론
+		 * AOP를 구현 할 때 스프링이 도움을 줌.
+		 * 사용자 요구사항을 수반하기 위해 주 업무 로직 외의 코드가 들어간다. 
+		 * 주 업무 로직을 만들기 위해 들어가게 되는 코드. 
+		 * 개발이나 운영에 따른 부가적인 코드로써 이부분은 사용자는 모른다. 
+		 * 그런 부분이 보조 업무, 곁다리 업무 등으로 볼 수 있다.
+		 * 그렇듯 사용자 관점, 운영자 관점, 개발자 관점의 로직이 있을 것이다.
+		 * 각각의 관점 단위의 업무를 나누어 프로그램을 진행하고 결합하는 것에 대한 방법론이 AOP
+		 * 로그처리, 권한, 보안처리, 트랜잭션 처리 등의 코드가 필요할 것이다.
+		 * 그러한 보조 로직은 앞이나 뒤, 혹은 위, 아래에 위치한다. 즉 주 로직의 처음과 끝부분에 있다.
+		 * 보조로직이 프락시를 사용하게 만든다. 이것이 cross-cutting Concern
+		 * 
+		 * 
+		 */
+		
 		ApplicationContext context = 
 				new AnnotationConfigApplicationContext(NewlecDIConfig.class); 
 //				new ClassPathXmlApplicationContext("diPrj/di/setting.xml");
