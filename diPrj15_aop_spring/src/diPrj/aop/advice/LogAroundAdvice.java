46e7f1b828887ae6192300d47fc6865ba8f0993b
sidekava <sidekava@gmail.com>
2020-08-19 22:36:09 +0900

    21강 aop 복습

---
+++ diPrj15_aop_spring/src/diPrj/aop/advice/LogAroundAdvice.java
@@ -0,0 +1,14 @@
+package diPrj.aop.advice;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class LogAroundAdvice implements MethodInterceptor {
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
