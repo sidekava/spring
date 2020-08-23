94ab68b21c7460c2ebfb21a434eafa2b984252b9
sidekava <sidekava@gmail.com>
2020-08-23 21:13:16 +0900

    21강 스프링으로 프록시 구현 언어만 익히도록 복습

--- diPrj15_aop_spring/src/diPrj/aop/Program.java
+++ diPrj15_aop_spring/src/diPrj/aop/Program.java
@@ -1,14 +1,9 @@
 package diPrj.aop;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import diPrj.aop.entity.Exam;
-import diPrj.aop.entity.NewlecExam;
 
 public class Program {
 	public static void main (String[] args) {
@@ -16,36 +11,36 @@ public class Program {
 //				new AnnotationConfigApplicationContext(NewlecDIConfig.class); 
 				new ClassPathXmlApplicationContext("diPrj/aop/setting.xml");
 		
-		Exam exam = new NewlecExam(1, 1, 1, 1);
-		
-		/*
-		 * 프락시는 가짜인데 실제처럼 로드해야 한다. 그래서 loader에 객체를 로드할 수 있도록 한다.
-		 */
-		
-		Exam proxy = (Exam) Proxy.newProxyInstance(NewlecExam.class.getClassLoader(), 
-				new Class[] {Exam.class},
-				new InvocationHandler() {
-					
-					@Override
-					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-						long start = System.currentTimeMillis();
-						
-						Object result = method.invoke(exam, args);
-						
-						long end = System.currentTimeMillis();
-						
-						String message = (end - start) + "ms 시간이 걸렸습니다";
-						System.out.println(message);
-						
-						return result;
-					}
-				}
-			
-					
-		);
+		Exam proxy = (Exam) context.getBean("proxy");
 		
 		System.out.printf("total is %d\n", proxy.total());
 		System.out.printf("total is %d\n", proxy.avg());
+//		Exam exam = new NewlecExam(1, 1, 1, 1);
+//		
+//		/*
+//		 * 프락시는 가짜인데 실제처럼 로드해야 한다. 그래서 loader에 객체를 로드할 수 있도록 한다.
+//		 */
+//		
+//		Exam proxy = (Exam) Proxy.newProxyInstance(NewlecExam.class.getClassLoader(), 
+//				new Class[] {Exam.class},
+//				new InvocationHandler() {
+//					
+//					@Override
+//					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//						long start = System.currentTimeMillis();
+//						
+//						Object result = method.invoke(exam, args);
+//						
+//						long end = System.currentTimeMillis();
+//						
+//						String message = (end - start) + "ms 시간이 걸렸습니다";
+//						System.out.println(message);
+//						
+//						return result;
+//					}
+//				}
+//		);
+		
 				
 	}
 
--- diPrj15_aop_spring/src/diPrj/aop/advice/LogAroundAdvice.java
+++ diPrj15_aop_spring/src/diPrj/aop/advice/LogAroundAdvice.java
@@ -8,7 +8,16 @@ public class LogAroundAdvice implements MethodInterceptor {
 	@Override
 	public Object invoke(MethodInvocation invocation) throws Throwable {
 		// TODO Auto-generated method stub
-		return null;
+		long start = System.currentTimeMillis();
+		
+		Object result = invocation.proceed();
+		
+		long end = System.currentTimeMillis();
+		
+		String message = (end - start) + "ms 시간이 걸렸습니다";
+		System.out.println(message);
+		
+		return result;
 	}
 	
 }
